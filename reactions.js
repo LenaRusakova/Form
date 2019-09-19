fa2feb9781e0ec9fc00a12e015de484c4073487d
LenaRusakova <55556858+LenaRusakova@users.noreply.github.com>
2019-09-19 23:57:00 +0500

    Add files via upload

---
+++ reactions.js
@@ -0,0 +1,33 @@
+function OnMouseOver(element) {
+    element.classList.add('mouseover');
+}
+
+function OnMouseOut(element) {
+    element.classList.remove('mouseover');
+}
+
+function ButtonMouseOver(element) {
+
+    element.classList.add('button_mouseover');
+}
+
+function ButtonMouseOut(element) {
+    element.classList.remove('button_mouseover');
+}
+
+function OnFocus(element) {
+    element.classList.add('focus');
+}
+
+function OnBlur(element) {
+    element.classList.remove('focus');
+}
+
+function ButtonFocus(element) {
+
+    element.classList.add('button_focus');
+}
+
+function ButtonBlur(element) {
+    element.classList.remove('button_focus');
+}
